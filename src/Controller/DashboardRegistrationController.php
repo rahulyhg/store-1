ebd5626978605f6e966edd3ba9a9a94b14106ea7
Serj Bliznyuk <serjbliznyuk@gmail.com>
2019-01-21 19:17:35 +0200

    Разработка dashboard/registration

--- src/Controller/DashboardRegistrationController.php
+++ src/Controller/DashboardRegistrationController.php
@@ -10,7 +10,7 @@ use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\Filesystem\Exception\IOExceptionInterface;
 
-use App\Entity\DashboardAuthors;
+use App\Entity\DashboardUsers;
 
 class DashboardRegistrationController extends MainDashboardController
 {
@@ -74,12 +74,12 @@ class DashboardRegistrationController extends MainDashboardController
         $session = new Session();
         $session->invalidate();
 
-        $path_authors_images = $this->getAppDir() . '/public/images/users/';
+        $path_user_images = $this->getAppDir() . '/public/images/users/';
 
         $image_token_name = $request->request->get('image_token_name');
 
         $em = $this->getDoctrine()->getManager();
-        $dashboard_authors = new DashboardAuthors();
+        $dashboard_users = new DashboardUsers();
 
         if (strcmp($image_token_name, '0') !== 0 && strlen($image_token_name) == 32) {
             $x_o = $request->request->get('image_x1');
@@ -87,22 +87,22 @@ class DashboardRegistrationController extends MainDashboardController
             $w_o = $request->request->get('image_w');
             $h_o = $request->request->get('image_h');
 
-            $author_image_file = $request->files->get('input_author_image_file');
-            $author_image_file->move($path_authors_images, $image_token_name);
-            $this->cropImage($path_authors_images . $image_token_name, $x_o, $y_o, $w_o, $h_o);
+            $user_image_file = $request->files->get('input_user_image_file');
+            $user_image_file->move($path_users_images, $image_token_name);
+            $this->cropImage($path_users_images . $image_token_name, $x_o, $y_o, $w_o, $h_o);
         } else {
             $image_token_name = 0;
         }
 
-        $dashboard_authors->setAuthorName($request->request->get('input_author_name'));
-        $dashboard_authors->setAuthorAbout($request->request->get('input_author_about'));
-        $dashboard_authors->setAuthorEmail($request->request->get('input_author_email'));
-        $dashboard_authors->setAuthorImage($image_token_name);
-        $dashboard_authors->setAuthorLogin($request->request->get('input_author_login'));
-        $dashboard_authors->setAuthorPassword($this->getPasswordHash($request->request->get('input_author_password')));
-        $dashboard_authors->setAuthorStatus(0);
+        $dashboard_users->setUserName($request->request->get('input_user_name'));
+        $dashboard_users->setUserPhone($request->request->get('input_user_phone'));
+        $dashboard_users->setUserEmail($request->request->get('input_user_email'));
+        $dashboard_users->setUserImage($image_token_name);
+        //$dashboard_users->setUserLogin($request->request->get('input_user_login'));
+        $dashboard_users->setUserPassword($this->getPasswordHash($request->request->get('input_user_password')));
+        $dashboard_users->setUserStatus(0);
 
-        $em->persist($dashboard_authors);
+        $em->persist($dashboard_users);
         $em->flush();
 
         return new RedirectResponse($this->generateUrl('dashboard_dashboard'));
