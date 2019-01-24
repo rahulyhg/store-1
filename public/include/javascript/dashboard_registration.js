caee5e9febc3705ead162d8f4a1d93eae028e955
Serj Bliznyuk <serjbliznyuk@gmail.com>
2019-01-24 16:52:47 +0200

    Разработка dashboard/registration

--- public/include/javascript/dashboard_registration.js
+++ public/include/javascript/dashboard_registration.js
@@ -4,11 +4,8 @@
 function serializeForm() {
   var form_data = {
     'name': $('#input_user_name').val(),
-    //'about': $('#input_author_about').val(),
     'phone': $('#input_user_phone').val(),
     'email': $('#input_user_email').val(),
-    'token': $('#image_token_name').val(),
-    //'login': $('#input_author_login').val(),
     'password': $('#input_user_password').val()
   };
   return form_data;
@@ -19,77 +16,9 @@ function serializeForm() {
 /* ##################################################################################### */
 function clearForm() {
   $('#input_user_name').val(null).trigger('blur');
-  //$('#input_author_about').val(null).trigger('blur');
   $('#input_user_email').val(null).trigger('blur');
   $('#input_user_phone').val(null).trigger('blur');
-  $('#input_user_image_file').val(null);
-  $('#input_user_image_filename').val(null).trigger('blur');
-  $('#input_token_name').val(null);
-  //$('#input_author_login').val(null).trigger('blur');
   $('#input_user_password').val(null).trigger('blur');
 }
 
-/* ##################################################################################### */
-// Снятие выделения на обрезаемом изображении
-/* ##################################################################################### */
-function releaseCrop() {
-  jcrop_api.release();
-  $('#crop').hide();
-}
 
-/* ##################################################################################### */
-// Отображения обрезки изображения
-// и вставка значение в input hidden
-/* ##################################################################################### */
-function showCoords(c) {
-  $('#image_x1').val(c.x);
-  $('#image_y1').val(c.y);
-  $('#image_x2').val(c.x2);
-  $('#image_y2').val(c.y2);
-
-  $('#image_w').val(c.w);
-  $('#image_h').val(c.h);
-  if (c.w > 0 && c.h > 0) {
-    $('#crop').show();
-  } else {
-    $('#crop').hide();
-  }
-}
-
-/* ##################################################################################### */
-//
-/* ##################################################################################### */
-function getImageOriginSize(img_src) {
-  var image = new Image();
-  image.onload = function() {
-    origin_image_height = image.height;
-    origin_image_width = image.width;
-  }
-  image.src = img_src;
-}
-
-/* ##################################################################################### */
-// Выводит выбранное изображение в форму обрезки
-// и устанавливает отношение сторон "квадрат"
-/* ##################################################################################### */
-function previewImage(input) {
-  $('#origin_image_field').html('<img class="responsive-img" id="origin_image" src="">');
-  if (input.files && input.files[0]) {
-    var file_reader = new FileReader();
-    file_reader.onload = function(e) {
-      $('#origin_image').attr('src', e.target.result).show();
-      getImageOriginSize(e.target.result);
-      $('#origin_image').Jcrop({
-        onChange: showCoords,
-        onSelect: showCoords
-      }, function() {
-        jcrop_api = this;
-        jcrop_api.setOptions({
-          aspectRatio: 1 / 1
-        });
-        jcrop_api.focus();
-      });
-    };
-    file_reader.readAsDataURL(input.files[0]);
-  }
-}
--- src/Controller/DashboardRegistrationController.php
+++ src/Controller/DashboardRegistrationController.php
@@ -31,80 +31,57 @@ class DashboardRegistrationController extends MainDashboardController
     /* ##################################################################################### */
     //
     /* ##################################################################################### */
-    private function cropImage($image, $x_o, $y_o, $w_o, $h_o)
-    {
-        if (($x_o < 0) || ($y_o < 0) || ($w_o <= 0) || ($h_o <= 0)) {
-            //echo "Некорректные входные параметры";
-            return false;
-        }
-
-        list($w_i, $h_i, $type) = getimagesize($image);
-        $types = array('', 'gif', 'jpeg', 'png');
-        $ext = $types[$type];
-
-        if ($ext) {
-            $func = 'imagecreatefrom' . $ext;
-            $img_i = $func($image);
-        } else {
-            //echo 'Некорректное изображение';
-            return false;
-        }
-
-        if ($x_o + $w_o > $w_i) {
-            $w_o = $w_i - $x_o;
-        }
-        if ($y_o + $h_o > $h_i) {
-            $h_o = $h_i - $y_o;
-        }
-
-        $img_o = imagecreatetruecolor($w_o, $h_o);
-
-        imagecopy($img_o, $img_i, 0, 0, $x_o, $y_o, $w_o, $h_o);
-
-        $func = 'image'.$ext;
-
-        $func($img_o, $image);
-    }
-
-    /* ##################################################################################### */
-    //
-    /* ##################################################################################### */
     public function registerUserAction(Request $request)
     {
         $session = new Session();
         $session->invalidate();
 
-        $path_user_images = $this->getAppDir() . '/public/images/users/';
-
-        $image_token_name = $request->request->get('image_token_name');
-
         $em = $this->getDoctrine()->getManager();
         $dashboard_users = new DashboardUsers();
 
-        if (strcmp($image_token_name, '0') !== 0 && strlen($image_token_name) == 32) {
-            $x_o = $request->request->get('image_x1');
-            $y_o = $request->request->get('image_y1');
-            $w_o = $request->request->get('image_w');
-            $h_o = $request->request->get('image_h');
-
-            $user_image_file = $request->files->get('input_user_image_file');
-            $user_image_file->move($path_users_images, $image_token_name);
-            $this->cropImage($path_users_images . $image_token_name, $x_o, $y_o, $w_o, $h_o);
+        if (strlen($request->request->get('input_user_name')) >= 3 || strlen($request->request->get('input_user_name')) < 50) {
+        	return $this->render('error_request.html.twig', ['translation' => $this->getTranslation()]);
         } else {
-            $image_token_name = 0;
+            $dashboard_users->setUserName($request->request->get('input_user_name'));
         }
-
-        $dashboard_users->setUserName($request->request->get('input_user_name'));
-        $dashboard_users->setUserPhone($request->request->get('input_user_phone'));
-        $dashboard_users->setUserEmail($request->request->get('input_user_email'));
-        $dashboard_users->setUserImage($image_token_name);
-        //$dashboard_users->setUserLogin($request->request->get('input_user_login'));
-        $dashboard_users->setUserPassword($this->getPasswordHash($request->request->get('input_user_password')));
+        
+        if ($this->checkPhoneNumber($request->request->get('input_user_phone')) == true) {
+            $dashboard_users->setUserPhone($request->request->get('input_user_phone'));
+         } else {
+         	return $this->render('error_request.html.twig', ['translation' => $this->getTranslation()]);
+         }
+         
+        if ($this->checkEmail($request->request->get('input_user_email')) == true) {
+            $dashboard_users->setUserEmail($request->request->get('input_user_email'));
+        } else {
+        	return $this->render('error_request.html.twig', ['translation' => $this->getTranslation()]);
+        }
+        
+        if (strlen($request->request->get('input_user_password')) >= 6 || strlen($request->request->get('input_user_password')) < 50) {
+        	$dashboard_users->setUserPassword($this->getPasswordHash($request->request->get('input_user_password')));
+        } else {
+            return $this->render('error_request.html.twig', ['translation' => $this->getTranslation()]);
+        }
+        
         $dashboard_users->setUserStatus(0);
 
         $em->persist($dashboard_users);
         $em->flush();
 
         return new RedirectResponse($this->generateUrl('dashboard_dashboard'));
+    }
+    
+    /* ##################################################################################### */
+    // Проверяет наличие в БД аккаунта с таким емейлом
+    /* ##################################################################################### */
+    public function checkEmailExists() {
+    	
+    }
+    
+    /* ##################################################################################### */
+    // Проверяет наличие в БД аккаунта с таким номером телефона
+    /* ##################################################################################### */
+    public function checkPhoneNumberExists() {
+    	
     }
 }
