3870daacfc7365adcd58492869466465a54039b5
Serj Bliznyuk <serjbliznyuk@gmail.com>
2019-01-31 19:24:38 +0200

    Разработка dashboard/dashboard

---
+++ src/Controller/DashboardModulesController.php
@@ -0,0 +1,31 @@
+<?php
+namespace App\Controller;
+
+use App\Controller\MainDashboardController;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+
+class DashboardModulesController extends MainDashboardController
+{
+    /* ##################################################################################### */
+    //
+    /* ##################################################################################### */
+    public function renderDashboardModulesTemplateAction()
+    {
+        if ($this->checkAuthorization() == true) {
+            $request = Request::createFromGlobals();
+            return $this->render('dashboard_modules.twig', array(
+            'translation' => $this->getTranslation(),
+            'profile' => $this->getProfile($request->cookies->get('user_id')),
+
+          ));
+        } else {
+            return $this->render('dashboard_authorization.twig', array(
+            'translation' => $this->getTranslation()
+          ));
+        }
+    }
+}
---
+++ src/Controller/DashboardRecommendationsController.php
@@ -0,0 +1,33 @@
+<?php
+namespace App\Controller;
+
+use App\Controller\MainDashboardController;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+
+//use App\Entity\DashboardAuthors;
+
+class DashboardRecommendationsController extends MainDashboardController
+{
+    /* ##################################################################################### */
+    //
+    /* ##################################################################################### */
+    public function renderDashboardRecommendationsTemplateAction()
+    {
+        if ($this->checkAuthorization() == true) {
+            $request = Request::createFromGlobals();
+            return $this->render('dashboard_recommendations.twig', array(
+            'translation' => $this->getTranslation(),
+            'profile' => $this->getProfile($request->cookies->get('user_id')),
+
+          ));
+        } else {
+            return $this->render('dashboard_authorization.twig', array(
+            'translation' => $this->getTranslation()
+          ));
+        }
+    }
+}
