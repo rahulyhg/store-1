7f7dd34dca2ecf575af1d9d9f0999d1d2c5c42ab
Serj Bliznyuk <serjbliznyuk@gmail.com>
2019-02-07 09:04:40 +0200

    Разработка dashboard/scripts

--- src/Controller/DashboardScriptsController.php
+++ src/Controller/DashboardScriptsController.php
@@ -4,9 +4,7 @@ namespace App\Controller;
 use App\Controller\MainDashboardController;
 
 use Symfony\Component\HttpFoundation\Request;
-//use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\JsonResponse;
-//use Symfony\Component\HttpFoundation\RedirectResponse;
 
 use App\Entity\StoreScripts;
 
