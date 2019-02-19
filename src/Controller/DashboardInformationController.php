e5e19173788f6c007049eb28995d976267848e83
Serj Bliznyuk <serjbliznyuk@gmail.com>
2019-02-19 23:42:03 +0200

    Разработка dashboard/settings/information

--- src/Controller/DashboardInformationController.php
+++ src/Controller/DashboardInformationController.php
@@ -5,6 +5,8 @@ use App\Controller\MainDashboardController;
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Filesystem\Exception\IOExceptionInterface;
 
 class DashboardInformationController extends MainDashboardController
 {
@@ -14,17 +16,17 @@ class DashboardInformationController extends MainDashboardController
     public function renderDashboardInformationTemplateAction()
     {
         if ($this->checkAuthorization() == true) {
-          $request = Request::createFromGlobals();
-          return $this->render('dashboard_settings.twig', array(
+            $request = Request::createFromGlobals();
+            return $this->render('dashboard_information.twig', array(
               'translation' => $this->getTranslation(),
               'authorization' => $this->checkAuthorization(),
               'profile' => $this->getProfile($request->cookies->get('user_id')),
-          ));
+            ));
         } else {
             return $this->render('dashboard_authorization.twig', array(
               'translation' => $this->getTranslation(),
               'authorization' => $this->checkAuthorization(),
-          ));
+            ));
         }
     }
 
@@ -33,15 +35,31 @@ class DashboardInformationController extends MainDashboardController
     /* ##################################################################################### */
     public function getInformationAction(Request $request)
     {
-      if ($this->checkAuthorization() == true) {
+        if ($this->checkAuthorization() == true) {
+            $fileSystem = new Filesystem();
+            $information_file_path = $this->getAppDir() . '/public/content/information.html';
 
-        return new JsonResponse();
-      } else {
-          return $this->render('dashboard_authorization.twig', array(
-            'translation' => $this->getTranslation(),
-            'authorization' => $this->checkAuthorization(),
-        ));
-      }
+            try {
+                //$fileSystem->mkdir(sys_get_temp_dir().'/'.random_int(0, 1000));
+
+            } catch (IOExceptionInterface $exception) {
+                $this->writeLog('App/Controller/DashboardInformationController::getInformationAction & Error read file: ' . $exception->getPath());
+            }
+
+            // get contents of a file into a string
+            $filename = "/usr/local/something.txt";
+            $handle = fopen($filename, "r");
+            $contents = fread($handle, filesize($filename));
+            fclose($handle);
+
+            return new JsonResponse();
+        } else {
+            $this->writeLog('App/Controller/DashboardInformationController::getInformationAction Authorization Error');
+            return $this->render('dashboard_authorization.twig', array(
+              'translation' => $this->getTranslation(),
+              'authorization' => $this->checkAuthorization(),
+            ));
+        }
     }
 
     /* ##################################################################################### */
@@ -49,15 +67,13 @@ class DashboardInformationController extends MainDashboardController
     /* ##################################################################################### */
     public function saveInformationAction(Request $request)
     {
-      if ($this->checkAuthorization() == true) {
-
-        return new JsonResponse();
-      } else {
-          return $this->render('dashboard_authorization.twig', array(
-            'translation' => $this->getTranslation(),
-            'authorization' => $this->checkAuthorization(),
-        ));
-      }
+        if ($this->checkAuthorization() == true) {
+            return new JsonResponse();
+        } else {
+            return $this->render('dashboard_authorization.twig', array(
+              'translation' => $this->getTranslation(),
+              'authorization' => $this->checkAuthorization(),
+            ));
+        }
     }
-
 }
