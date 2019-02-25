0c5edf09b3ed8fe54a3cbb19b2b4b7d86fea3825
Serj Bliznyuk <serjbliznyuk@gmail.com>
2019-02-25 23:16:27 +0200

    Разработка dashboard/settings/meta

---
+++ public/extras/javascript/dashboard_meta.js
@@ -0,0 +1,79 @@
+/* ##################################################################################### */
+//
+/* ##################################################################################### */
+function getInformation(language_id) {
+  $.ajax({
+    url: get_information_action_url,
+    type: "POST",
+    cache: true,
+    async: true,
+    data: {
+      'request': true,
+      'language_id': language_id
+    },
+    dataType: "json",
+    beforeSend: function() {
+      $('#preloader').show();
+    },
+    success: function(response) {
+      if (response.status == true) {
+        if (response.newfile == true) {
+          elegant_alert.success(alert_create_new_file);
+          $('#input_information_text').val(null);
+          M.updateTextFields();
+          M.textareaAutoResize($('#input_information_text'));
+        } else {
+          $('#input_information_text').val(response.content);
+          M.updateTextFields();
+          M.textareaAutoResize($('#input_information_text'));
+        }
+      } else {
+        elegant_alert.error(error_get_information);
+      }
+    },
+    complete: function() {
+      $('#preloader').hide();
+      //M.textareaAutoResize($('#input_information_text'));
+      //M.updateTextFields();
+    },
+    error: function(xhr) {
+      elegant_alert.error(error_get_information);
+      writeLog('App/Javascript/DashboardInformation::getInformation');
+    }
+  });
+}
+
+/* ##################################################################################### */
+//
+/* ##################################################################################### */
+function saveInformation(language_id, information_content) {
+  $.ajax({
+    url: save_information_action_url,
+    type: "POST",
+    cache: true,
+    async: true,
+    data: {
+      'request': true,
+      'language_id': language_id,
+      'information_content': information_content
+    },
+    dataType: "json",
+    beforeSend: function() {
+      $('#preloader').show();
+    },
+    success: function(response) {
+      if (response.status == true) {
+        elegant_alert.success(alert_save_information);
+      } else {
+        elegant_alert.error(error_save_information);
+      }
+    },
+    complete: function() {
+      $('#preloader').hide();
+    },
+    error: function(xhr) {
+      elegant_alert.error(error_save_information);
+      writeLog('App/Javascript/DashboardInformation::saveInformation');
+    }
+  });
+}
--- src/Controller/DashboardMetaController.php
+++ src/Controller/DashboardMetaController.php
@@ -16,10 +16,11 @@ class DashboardMetaController extends MainDashboardController
     {
         if ($this->checkAuthorization() == true) {
           $request = Request::createFromGlobals();
-          return $this->render('dashboard_settings.twig', array(
+          return $this->render('dashboard_meta.twig', array(
               'translation' => $this->getTranslation(),
               'authorization' => $this->checkAuthorization(),
               'profile' => $this->getProfile($request->cookies->get('user_id')),
+              'languages' => $this->getAllLanguages(),
           ));
         } else {
             return $this->render('dashboard_authorization.twig', array(
