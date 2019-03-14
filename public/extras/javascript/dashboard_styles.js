5c82574ddf8da0b50fe92b6365de115b355a375e
Serj Bliznyuk <serjbliznyuk@gmail.com>
2019-03-15 00:15:29 +0200

    Разработка dashboard/settings/styles

--- public/extras/javascript/dashboard_styles.js
+++ public/extras/javascript/dashboard_styles.js
@@ -1,20 +1,33 @@
-var colors_pallete = {
-
-};
-
-function openColorsPalette(input) {
+/* ##################################################################################### */
+//
+/* ##################################################################################### */
+function openColorsPalette() {
   var elem = $('#modal_colors_palette');
   var instance = M.Modal.getInstance(elem);
   instance.open();
+}
 
-  input[0].value = "skdjfhksdjfhksdh";
-  M.updateTextFields();
+function preselectColor(color) {
+  selected_color = color;
+}
 
-  console.log(input);
+function unselectColors() {
+  $('.color-palette').each(function() {
+    $(this).removeClass('z-depth-5');
+  });
+}
+
+/* ##################################################################################### */
+function serializeForm() {
 
 }
 
-function preselectColor(selected_color) {
-  console.log(selected_color);
-  elegant_alert.success(selected_color[0].attributes.color.value);
+/* ##################################################################################### */
+function getStyles() {
+
+}
+
+/* ##################################################################################### */
+function saveStyles(form) {
+
 }
