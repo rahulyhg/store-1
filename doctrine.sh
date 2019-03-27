05eb72d132f16567864cfd0d5a3894f7de7048fa
Serj Bliznyuk <serjbliznyuk@gmail.com>
2019-03-27 10:42:42 +0200

    Реализована очистка файлов перед генерацией

--- doctrine.sh
+++ doctrine.sh
@@ -2,7 +2,10 @@
 
 echo "Start getting mapping and entities"
 
+rm -rf config/doctrine/*.yaml
 php bin/console doctrine:mapping:import App\\Entity yaml --path=config/doctrine
+
+rm -rf src/Entity/*.php
 php bin/console doctrine:mapping:import App\\Entity annotation --path=src/Entity
 php bin/console make:entity --regenerate App
 
